8f569b1c8cf5c76f5c5f44057be46152923611b5
utgak <degtyaryov.2002@mail.ru>
2021-10-19 22:18:33 +0300

    queue contract

--- queue.sol
+++ queue.sol
@@ -1,7 +1,9 @@
 pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
-contract Wallet {
+contract Queue {
+    string[] public queue;
+
     constructor() public {
         require(tvm.pubkey() != 0, 101);
         require(msg.pubkey() == tvm.pubkey(), 102);
@@ -15,5 +17,14 @@ contract Wallet {
 		_;
 	}
 
+    function callNext() public checkOwnerAndAccept {
+        for (uint i; i < queue.length - 1; i++) {
+            queue[i] = queue[i + 1];
+        }
+        queue.pop();
+    }
 
+    function getInQueue(string name) public checkOwnerAndAccept {
+        queue.push(name);
+    }
 }
